ca452470753e90797198550f9859f292741f9c31
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-09-02 11:14:05 +0200

    Add h2 and h3 component

---
+++ src/components/HeadlineH2.js
@@ -0,0 +1,5 @@
+const HeadlineH2 = ({ text }) => {
+  return <h2 className="text-xl">{text}</h2>;
+};
+
+export default HeadlineH2;
---
+++ src/components/HeadlineH3.js
@@ -0,0 +1,5 @@
+const H3 = ({ text }) => {
+  return <h3 className="ml-6 mb-6 font-bold text-black text-xl">{text}</h3>;
+};
+
+export default H3;

7a490b964578beb880e9f9dce5f663e984c25b3a
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-09-06 09:51:17 +0200

    Add Card component

--- src/templates/ClassCarousel.js
+++ src/templates/ClassCarousel.js
@@ -1,12 +1,12 @@
 import { ScrollMenu } from "react-horizontal-scrolling-menu";
-import FeaturedCard from "../components/FeaturedCard";
+import Card from "../components/Card";
 
 const ClassCarousel = ({ data }) => {
   return (
-    <div className="mr-4 overflow-hidden grid-rows-1">
+    <div className="mr-4 ml-6 overflow-hidden">
       <ScrollMenu>
         {data.map((item, index) => (
-          <FeaturedCard data={item} key={index} />
+          <Card data={item} key={index} />
         ))}
       </ScrollMenu>
     </div>

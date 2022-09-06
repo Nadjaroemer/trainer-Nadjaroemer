549dce5a32881a2fd547c09f8fda5a44ee8dc6dd
Nadja Rømer Hesselbjerg <nadja.kuti@gmail.com>
2022-09-06 10:47:18 +0200

    Add ClassDetailsHeader component

---
+++ src/components/ClassDetailsHeader.js
@@ -0,0 +1,37 @@
+import { AiFillStar } from "react-icons/ai";
+import { useContext } from "react";
+import { StateContext } from "../context/Context";
+
+const ClassDetailsHeader = ({ data }) => {
+  const { userData, setUserData } = useContext(StateContext);
+  return (
+    <header className="-mt-20">
+      <div
+        className="flex flex-col justify-end h-80 object-cover"
+        style={{
+          backgroundImage: `url(${data.asset.url})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <h1 className="font-bold text-3xl ml-6 mb-4  text-secondary ">
+          {data.className}
+        </h1>
+        <div className="flex ml-6 mb-4 items-center">
+          <AiFillStar className="text-secondary mr-2" />
+          <AiFillStar className="text-secondary mr-2" />
+          <AiFillStar className="text-secondary mr-2" />
+          <AiFillStar className="text-secondary mr-2" />
+          <AiFillStar className="text-secondary mr-2" />
+          <p className="text-secondary">5/5</p>
+          {userData && (
+            <button className="text-center uppercase font-bold border-secondary border-2 text-secondary py-3 px-7 ml-6">
+              Rate
+            </button>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default ClassDetailsHeader;
